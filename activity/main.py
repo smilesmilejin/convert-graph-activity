3da46f76aba1f0fa968acc145bad7e67b412e80e
Xinshuang Jin <jinxinshuang@gmail.com>
2025-10-04 23:03:41 -0500

    feat: implement convert_graph function and pass all tests

--- activity/main.py
+++ activity/main.py
@@ -4,3 +4,45 @@ def convert_graph(edge_lst):
     Assume the list of edges represents a directed graph.
     """
     pass
+
+    # fix max number of edge_list
+    # matrix will be max * max
+    # make them all 0s
+
+    # for each edge
+        # make the correponding matrix 1
+
+    # return matrix
+
+
+    if not edge_lst:
+        return []
+    
+
+    n = float('-inf')
+
+    for edge in edge_lst:
+        n = max(n, max(edge))
+
+    n = n + 1
+
+    matrix = [[0 for j in range(n)] for i in range(n)]
+    # print(matrix)
+
+    # matrix = []
+    # for i in range(n):
+    #     sub = []
+    #     for j in range(n):
+    #         sub.append(0)
+
+    #     matrix.append(sub)
+
+    print(matrix)
+
+    for row, col in edge_lst:
+        # print(row)
+        # print(col)
+        matrix[row][col] = 1
+
+    return matrix
+
